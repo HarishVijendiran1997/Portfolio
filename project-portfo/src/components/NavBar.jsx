6ddb77c5ebb74eb551e006c80aed33c1071fe766
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-22 17:19:37 +0530

    add: Hero section to portfolio

--- project-portfo/src/components/NavBar.jsx
+++ project-portfo/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import heroImage from "../assets/HeroImage.png"
 
 const NavBar = () => {
     const [nav, setNav] = useState(false);
@@ -9,9 +10,9 @@ const NavBar = () => {
     };
 
     return (
-        <div className="bg-black text-gray-400 max-w-[1200px] mx-auto h-24 flex justify-between items-center px-6">
+        <div className="bg-black text-gray-400 max-w-full mx-auto h-24 flex justify-between items-center sm:px-20 px-6">
             {/* Logo */}
-            <h1 className="text-3xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-orange-500 to-pink-500">Harish Vijendiran</h1>
+            <h1 className="text-3xl font-bold bg-gradient-to-r bg-pink-500 text-transparent bg-clip-text from-blue-500  to-pink-500">Harish Vijendiran</h1>
 
             {/* Desktop Menu */}
             <ul className="hidden md:flex space-x-8">
@@ -19,7 +20,6 @@ const NavBar = () => {
                 <li className="p-3 hover:text-white"><a href="#work">Work</a></li>
                 <li className="p-3 hover:text-white"><a href="#contact">Contact</a></li>
             </ul>
-
             {/* Mobile Menu Toggle */}
             <div onClick={handleNav} className="md:hidden cursor-pointer text-white">
                 {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
@@ -27,7 +27,7 @@ const NavBar = () => {
 
             {/* Mobile Sidebar */}
             <div className={`fixed top-0 left-0 h-full w-[60%] bg-[#202121] transition-all duration-500 z-50 ${nav ? "left-0" : "left-[-100%]"}`}>
-                <h1 className="text-3xl bg-gradient-to-r text-transparent bg-clip-text from-orange-500 to-pink-500 m-4 ">Harish Vijendiran</h1>
+                <h1 className="text-3xl bg-gradient-to-r text-transparent bg-clip-text from-blue-500 to-pink-500 m-4 ">Harish Vijendiran</h1>
                 <ul className="p-8 text-xl">
                     <li className="p-3 hover:bg-gray-700"><a href="#about">About</a></li>
                     <li className="p-3 hover:bg-gray-700"><a href="#work">Work</a></li>
